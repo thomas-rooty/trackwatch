d74bdee31bb86b649469c1e3b1c9bc5e1eb5692c
thomas-rooty <tc.caron91@gmail.com>
2023-03-10 14:46:57 +0100

    Add/Remove show for user

--- components/cards/ShowCard.tsx
+++ components/cards/ShowCard.tsx
@@ -1,34 +1,56 @@
 import styles from './showcard.module.css';
 import Image from "next/legacy/image";
-import {useRouter} from "next/navigation";
-import {Show} from "@/types/show.interface";
-import {useAuthStore} from "@/stores/auth";
-import {appendShowToUser} from "@/utils/supabaseFunctions";
+import { useRouter } from "next/navigation";
+import { Show } from "@/types/show.interface";
+import { useAuthStore } from "@/stores/auth";
+import { appendShowToUser, checkIfShowIsSaved, removeShowFromUser } from "@/utils/supabaseFunctions";
+import {useState, useEffect, SetStateAction} from "react";
 
-const ShowCard = ({show}: { show: Show }) => {
-  const router = useRouter()
+const ShowCard = ({ show }: { show: Show }) => {
+  const router = useRouter();
+  const [isSaved, setIsSaved] = useState<boolean>(false);
 
   // Get user from store
-  const user = useAuthStore(state => state.user)
-  const setUser = useAuthStore(state => state.setUser)
+  const user = useAuthStore(state => state.user);
+
+  useEffect(() => {
+    const checkSavedShow = async () => {
+      const saved = await checkIfShowIsSaved(show.id, user?.id);
+      setIsSaved(saved as SetStateAction<boolean>);
+    };
+    checkSavedShow();
+  }, [show.id, user]);
 
   // Function that redirects to /discover/[id] when a show is clicked
   const handleShowClick = () => {
-    console.log('Redirect to :' + show.id)
-    router.push(`/show?id=${show.id}`)
-  }
+    router.push(`/show?id=${show.id}`);
+  };
 
-  // Function that appends show id to user's saved_shows array
-  const handleAddClick = () => {
-    appendShowToUser(show.id, user?.id)
-  }
+  // Function that appends/removes show id to/from user's saved_shows array
+  const handleAddClick = async () => {
+    if (isSaved) {
+      // Remove the show from saved shows
+      await removeShowFromUser(show.id, user?.id);
+      setIsSaved(false);
+    } else {
+      // Add the show to saved shows
+      await appendShowToUser(show.id, user?.id);
+      setIsSaved(true);
+    }
+  };
 
   return (
     <div className={styles.container}>
-      <div className={styles.addBtn} onClick={() => handleAddClick()}>
-        <span className={styles.addBtnText}>Add</span>
-      </div>
-      <div className={styles.showCard} onClick={() => handleShowClick()}>
+      {isSaved ? (
+        <div className={styles.addBtn} onClick={handleAddClick}>
+          <span className={styles.addBtnText}>Remove</span>
+        </div>
+      ) : (
+        <div className={styles.addBtn} onClick={handleAddClick}>
+          <span className={styles.addBtnText}>Add</span>
+        </div>
+      )}
+      <div className={styles.showCard} onClick={handleShowClick}>
         <Image
           src={`https://image.tmdb.org/t/p/w500${show.poster_path}`}
           alt={show.name}
@@ -36,14 +58,14 @@ const ShowCard = ({show}: { show: Show }) => {
           width={165}
           height={280}
         />
-        <div className={styles.overlay}/>
+        <div className={styles.overlay} />
         <div className={styles.showDetails}>
           <h3 className={styles.showTitle}>{show.name}</h3>
           <span className={styles.showVote}>{show.vote_count} votes</span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ShowCard
+export default ShowCard;
--- utils/supabaseFunctions.tsx
+++ utils/supabaseFunctions.tsx
@@ -33,6 +33,59 @@ export const appendShowToUser = async (showId: number, userId: string | undefine
     console.error(updateError)
     return
   }
+}
 
-  console.log(`Show ${showId} added to user ${userId}'s saved shows`)
+// Function that remove show from user's saved_shows array
+export const removeShowFromUser = async (showId: number, userId: string | undefined) => {
+  // Get current user data from database
+  const {data, error} = await supabase
+    .from('users')
+    .select('saved_shows')
+    .eq('id', userId)
+    .single()
+
+  if (error) {
+    console.error(error)
+    return
+  }
+
+  // Check if showId already exists in saved_shows array
+  const savedShows = data.saved_shows || []
+  if (!savedShows.includes(showId)) {
+    console.log(`Show ${showId} is not in user ${userId}'s saved shows`)
+    return
+  }
+
+  // Remove showId from saved_shows array
+  const newSavedShows = savedShows.filter((show: number) => show !== showId)
+
+  // Update user data in database
+  const {error: updateError} = await supabase
+    .from('users')
+    .update({saved_shows: newSavedShows})
+    .eq('id', userId)
+
+  if (updateError) {
+    console.error(updateError)
+    return
+  }
+}
+
+// Function that check if showId is in user's saved_shows array
+export const checkIfShowIsSaved = async (showId: number, userId: string | undefined) => {
+  // Get current user data from database
+  const {data, error} = await supabase
+    .from('users')
+    .select('saved_shows')
+    .eq('id', userId)
+    .single()
+
+  if (error) {
+    console.error(error)
+    return
+  }
+
+  // Check if showId already exists in saved_shows array
+  const savedShows = data.saved_shows || []
+  return !!savedShows.includes(showId);
 }
