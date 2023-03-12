592547b5ee346ed2ced0db0d266da1a723ab9468
thomas-rooty <essainono1@gmail.com>
2023-03-12 01:57:59 +0100

    Styled selection mode

--- components/cards/SavedShowCard.tsx
+++ components/cards/SavedShowCard.tsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import Image from 'next/image'
 import styles from './savedshowcard.module.css'
-import Selected from '@/public/icons/selected.png'
-import SelectedActive from '@/public/icons/selected_active.png'
+import CheckAnimation from '@/public/icons/check.gif'
+import CheckStatic from '@/public/icons/check_static.png'
 
 interface SavedShowCardProps {
   showId: number
@@ -22,6 +22,7 @@ const SavedShowCard = ({ showId, selectMode, setSelectedShows }: SavedShowCardPr
   })
 
   const [isSelected, setIsSelected] = useState(false)
+  const [showAnimatedCheckIcon, setShowAnimatedCheckIcon] = useState(false)
 
   useEffect(() => {
     const getShowDetails = async () => {
@@ -32,36 +33,45 @@ const SavedShowCard = ({ showId, selectMode, setSelectedShows }: SavedShowCardPr
     getShowDetails()
   }, [showId])
 
+  // Depending on selectMode, either redirect to show page or add/remove show from selectedShows array
   const handleShowClick = () => {
     if (selectMode) {
-      alert("Can't view show details in select mode !")
+      // Add to selectedShows array
+      setIsSelected((prev) => !prev)
+      setShowAnimatedCheckIcon(true)
+      setTimeout(() => {
+        setShowAnimatedCheckIcon(false)
+        if (!isSelected && setSelectedShows) {
+          setSelectedShows((prev) => [...prev, showId])
+        } else if (isSelected && setSelectedShows) {
+          setSelectedShows((prev) => prev.filter((id: number) => id !== showId))
+        }
+      }, 2000)
     } else {
+      // Redirect to show page
       router.push(`/show?id=${show.id}`)
-    }
-  }
-
-  const handleSelectClick = () => {
-    setIsSelected((prev) => !prev)
-    if (!isSelected && setSelectedShows) {
-      setSelectedShows((prev) => [...prev, showId])
-    } else if (isSelected && setSelectedShows) {
-      setSelectedShows((prev) => prev.filter((id: number) => id !== showId))
     }
   }
 
   return (
     <div className={styles.container}>
       {selectMode && (
-        <div className={styles.selectBtn} onClick={handleSelectClick}>
-          <Image src={isSelected ? SelectedActive : Selected} alt={'Select Btn'} width={30} height={30} />
+        <div className={styles.selectBtn}>
+          {isSelected && (
+            showAnimatedCheckIcon ? (
+              <Image src={CheckAnimation} alt={'Check Icon'} width={100} height={100} className={styles.checkIcon} />
+            ) : (
+              <Image src={CheckStatic} alt={'Check Icon'} width={100} height={100} className={styles.checkIcon} />
+            )
+          )}
         </div>
       )}
-      <div className={styles.showCard} onClick={handleShowClick}>
+      <div className={selectMode ? styles.showCardSelect : styles.showCard} onClick={handleShowClick}>
         <Image src={`https://image.tmdb.org/t/p/w500${show.poster_path}`} alt={show.name} className={styles.showPoster} width={160} height={280} />
         {!selectMode ?
           <div className={styles.overlay} />
           :
-          <div className={styles.overlayDarken} />
+          !isSelected && <div className={styles.overlayDarken}></div>
         }
         <div className={styles.showDetails}>
           <h3 className={styles.showTitle}>{show.name}</h3>
