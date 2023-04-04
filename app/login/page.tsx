3fbf6c3e0ca134c9bba62616be60595daeead621
thomas.caron <tc.caron91@gmail.com>
2023-04-04 12:05:07 +0200

    Back to home button on forms page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,10 +2,15 @@
 import styles from './login.module.css'
 import LoginForm from '@/components/forms/LoginForm'
 import Image from 'next/legacy/image'
+import {ButtonSecondary} from "@/components/buttons/Buttons";
+import Link from "next/link";
 
 const LoginPage = () => {
   return (
     <div className={styles.container}>
+      <Link href={'/'}>
+        <ButtonSecondary>{'Back to home'}</ButtonSecondary>
+      </Link>
       <Image src="/img/background.jpg" className={styles.backgroundImg} alt="background" layout="fill" objectFit="cover" />
       <LoginForm />
     </div>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,10 +2,15 @@
 import styles from './signup.module.css'
 import RegisterForm from '@/components/forms/RegisterForm'
 import Image from 'next/legacy/image'
+import Link from "next/link";
+import {ButtonSecondary} from "@/components/buttons/Buttons";
 
 const RegisterPage = () => {
   return (
     <div className={styles.container}>
+      <Link href={'/'}>
+        <ButtonSecondary>{'Back to home'}</ButtonSecondary>
+      </Link>
       <Image src="/img/background2.jpg" className={styles.backgroundImg} alt="background" layout="fill" objectFit="cover" />
       <RegisterForm />
     </div>
