dbe4d47f7bf9c15eb0d683ff7bf2d1ee28ef4795
thomas.caron <tc.caron91@gmail.com>
2023-04-04 16:32:13 +0200

    Added a calendar

---
+++ app/calendar/page.tsx
@@ -0,0 +1,66 @@
+'use client'
+import { useState, useEffect } from 'react'
+import { useAuthStore } from '@/stores/auth'
+import Sidebar from '@/components/sidebar/Sidebar'
+import styles from './calendar.module.css'
+import CalendarComponent from '@/components/calendar/CalendarComponent'
+
+const Calendar = () => {
+  const user = useAuthStore((state) => state.user)
+  const [loaded, setLoaded] = useState(false)
+  const [savedShows, setSavedShows] = useState<number[]>([])
+  const [savedShowsDetails, setSavedShowsDetails] = useState<any[]>([])
+  const [events, setEvents] = useState<any[]>([])
+
+  // Ensure that user is loaded before rendering
+  useEffect(() => {
+    setLoaded(true)
+  }, [])
+
+  // Update savedShows when user changes
+  useEffect(() => {
+    setSavedShows(user?.saved_shows ?? [])
+  }, [user])
+
+  // Fetch details for saved shows array
+  useEffect(() => {
+    const TMDB_API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY
+
+    async function fetchShowDetails(show: number) {
+      const response = await fetch(`https://api.themoviedb.org/3/tv/${show}?api_key=${TMDB_API_KEY}`)
+      const data = await response.json()
+      if (data.next_episode_to_air) {
+        setSavedShowsDetails((prev) => [...prev, data])
+      }
+    }
+
+    savedShows.forEach((show) => fetchShowDetails(show))
+  }, [savedShows])
+
+  useEffect(() => {
+    // Build events array
+    const events = savedShowsDetails.map((show) => {
+      const { name, next_episode_to_air } = show
+      const { air_date } = next_episode_to_air
+      const date = new Date(air_date)
+      return {
+        title: name,
+        date,
+      }
+    })
+    setEvents(events)
+    console.log(events)
+  }, [savedShowsDetails])
+
+  return (
+    <div className={styles.container}>
+      <Sidebar />
+      <div className={styles.sectionTitleContainer}>
+        <h1 className={styles.sectionTitle}>Calendar of events</h1>
+      </div>
+      {loaded && <CalendarComponent events={events} />}
+    </div>
+  )
+}
+
+export default Calendar
---
+++ components/calendar/CalendarComponent.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react'
+import styles from './calendar.module.css'
+import ArrowLeft from '@/public/icons/arrow_left.png'
+import ArrowRight from '@/public/icons/arrow_right.png'
+import Image from 'next/legacy/image'
+
+type Event = {
+  id: number
+  title: string
+  date: Date
+}
+
+type Props = {
+  events: Event[]
+}
+
+const Calendar: React.FC<Props> = ({ events }) => {
+  const [currentMonth, setCurrentMonth] = useState(new Date().getMonth())
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear())
+
+  const weeksInMonth = (month: number, year: number) => {
+    const lastDateOfMonth = new Date(year, month + 1, 0)
+    const daysInMonth = lastDateOfMonth.getDate()
+    const firstDateOfMonth = new Date(year, month, 1)
+    const firstDayOfMonth = firstDateOfMonth.getDay()
+    const daysInFirstWeek = 7 - firstDayOfMonth
+    const remainingDays = daysInMonth - daysInFirstWeek
+    return Math.ceil(remainingDays / 7) + 1
+  }
+
+  const weeks = []
+  const numWeeksInMonth = weeksInMonth(currentMonth, currentYear)
+
+  for (let weekIndex = 1; weekIndex <= numWeeksInMonth; weekIndex++) {
+    const week = []
+    for (let dayIndex = 1; dayIndex <= 7; dayIndex++) {
+      const dayOfMonth = (weekIndex - 1) * 7 + dayIndex - (7 - new Date(currentYear, currentMonth, 1).getDay())
+      const date = new Date(currentYear, currentMonth, dayOfMonth)
+      week.push(date)
+    }
+    weeks.push(week)
+  }
+
+  const prevMonth = () => {
+    setCurrentMonth(currentMonth === 0 ? 11 : currentMonth - 1)
+    setCurrentYear(currentMonth === 0 ? currentYear - 1 : currentYear)
+  }
+
+  const nextMonth = () => {
+    setCurrentMonth(currentMonth === 11 ? 0 : currentMonth + 1)
+    setCurrentYear(currentMonth === 11 ? currentYear + 1 : currentYear)
+  }
+
+  return (
+    <div className={styles.calendar}>
+      <div className={styles.header}>
+        <button className={styles.button} onClick={prevMonth}>
+          <Image src={ArrowLeft} alt="Previous month" width={26} height={26} />
+        </button>
+        <h2 className={styles.title}>
+          {new Date(currentYear, currentMonth).toLocaleDateString('en-US', {
+            month: 'long',
+            year: 'numeric',
+          })}
+        </h2>
+        <button className={styles.button} onClick={nextMonth}>
+          <Image src={ArrowRight} alt="Next month" width={26} height={26} />
+        </button>
+      </div>
+      {weeks.map((week, index) => (
+        <div className={styles.week} key={index}>
+          {week.map((date) => (
+            <div className={`${styles.day} ${date.getMonth() === currentMonth ? styles.currentMonth : styles.otherMonth}`} key={date.toDateString()}>
+              <div className={styles.date}>{date.getDate()}</div>
+              <div className={styles.events}>
+                {events
+                  .filter((event) => event.date.toDateString() === date.toDateString())
+                  .map((event) => (
+                    <div key={event.id} className={styles.event}>
+                      {event.title}
+                    </div>
+                  ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Calendar
