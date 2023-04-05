49de948e274b7ffc4c343cf492e60efb99b18d3e
thomas.caron <tc.caron91@gmail.com>
2023-04-05 15:22:22 +0200

    Fixed hydration errors on account page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,42 +1,13 @@
 'use client'
-import { useEffect, useState } from 'react'
-import type { User } from '@/types/user.interface'
-import { useRouter } from 'next/navigation'
-import { useUserStore } from '@/stores/user'
-import { supabase } from '@/utils/supabase'
 import styles from './account.module.css'
 import Sidebar from '@/components/sidebar/Sidebar'
 import UserProfile from '@/components/profile/UserProfile'
 
 const Account = () => {
-  const [user, setUser] = useState<User | null>(null)
-  const router = useRouter()
-  const { user: currentUser } = useUserStore()
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const { data: user, error } = await supabase.from('users').select('*').eq('email', currentUser?.email).single()
-
-        if (error) throw new Error(error.message)
-
-        setUser(user)
-      } catch (error: any) {
-        console.error(error.message)
-        // If there was an error fetching the user's information, we should redirect them back to the login page.
-        router.push('/login')
-      }
-    }
-
-    if (currentUser) {
-      fetchUser()
-    }
-  }, [currentUser, router])
-
   return (
     <div className={styles.container}>
       <Sidebar />
-      {user && <UserProfile user={user} />}
+      <UserProfile />
     </div>
   )
 }
--- components/profile/UserProfile.tsx
+++ components/profile/UserProfile.tsx
@@ -1,63 +1,73 @@
 import styles from './userprofile.module.css'
-import type {User} from "@/types/user.interface"
-import {ButtonMain, ButtonSecondary} from "@/components/buttons/Buttons";
-import Image from "next/legacy/image";
+import { ButtonMain, ButtonSecondary } from '@/components/buttons/Buttons'
+import Image from 'next/legacy/image'
+import { useEffect, useState } from 'react'
+import { useUserStore } from '@/stores/user'
 
-interface UserProfileProps {
-  user: User
-}
+const UserProfile = () => {
+  const user = useUserStore((state) => state.user)
+  const [loaded, setLoaded] = useState(false)
 
-const UserProfile = ({user}: UserProfileProps) => {
+  useEffect(() => {
+    if (user) {
+      setLoaded(true)
+    }
+  }, [user])
+
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Welcome {user.name}</h1>
-      <div className={styles.ppSection}>
-        <div className={styles.avatarUpper}>
-          <p className={styles.description}>Avatar</p>
-          <div>
-            <Image
-              className={styles.avatar}
-              src={(user.avatar_url as string)}
-              alt="Avatar"
-              width={100}
-              height={100}
-            />
+    <>
+      {loaded && (
+        <div className={styles.container}>
+          <h1 className={styles.title}>Welcome {user?.name}</h1>
+          <div className={styles.ppSection}>
+            <div className={styles.avatarUpper}>
+              <p className={styles.description}>Avatar</p>
+              <div>
+                <Image
+                  className={styles.avatar}
+                  src={user?.avatar_url as string}
+                  alt="Avatar"
+                  width={100}
+                  height={100}
+                />
+              </div>
+            </div>
+            <div className={styles.avatarLower}>
+              <div className={styles.ppUpload}>
+                <ButtonSecondary>Upload</ButtonSecondary>
+              </div>
+            </div>
+          </div>
+          <div className={styles.infoSection}>
+            <div className={styles.infoUpper}>
+              <p className={styles.description}>Name</p>
+              <div className={styles.infoInput}>
+                <input type="text" value={user?.name} />
+              </div>
+            </div>
+          </div>
+          <div className={styles.infoSection}>
+            <div className={styles.infoUpper}>
+              <p className={styles.description}>Email</p>
+              <div className={styles.infoInput}>
+                <input type="email" value={user?.email} />
+              </div>
+            </div>
+          </div>
+          <div className={styles.infoSection}>
+            <div className={styles.infoUpper}>
+              <p className={styles.description}>Password</p>
+              <div className={styles.infoInput}>
+                <input type="password" value={''} />
+              </div>
+            </div>
+          </div>
+          <div className={styles.saveSection}>
+            <ButtonMain>Save changes</ButtonMain>
           </div>
         </div>
-        <div className={styles.avatarLower}>
-          <div className={styles.ppUpload}>
-            <ButtonSecondary>Upload</ButtonSecondary>
-          </div>
-        </div>
-      </div>
-      <div className={styles.infoSection}>
-        <div className={styles.infoUpper}>
-          <p className={styles.description}>Name</p>
-          <div className={styles.infoInput}>
-            <input type="text" value={user.name}/>
-          </div>
-        </div>
-      </div>
-      <div className={styles.infoSection}>
-        <div className={styles.infoUpper}>
-          <p className={styles.description}>Email</p>
-          <div className={styles.infoInput}>
-            <input type="email" value={user.email}/>
-          </div>
-        </div>
-      </div>
-      <div className={styles.infoSection}>
-        <div className={styles.infoUpper}>
-          <p className={styles.description}>Password</p>
-          <div className={styles.infoInput}>
-            <input type="password" value={''}/>
-          </div>
-        </div>
-      </div>
-      <div className={styles.saveSection}>
-        <ButtonMain>Save changes</ButtonMain>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
 
